72177d98d7f21f67a64d33c46e3159312234dbf3
Calvin <calvin@mona.co>
2018-02-27 18:09:17 +0800

    Added files

---
+++ dumpkey.js
@@ -0,0 +1,9 @@
+const keythereum = require('keythereum');
+
+const dumpPrivKey = (address, password, dataDir) => {
+  const keyObject = keythereum.importFromFile(address, dataDir);
+  const privKey = keythereum.recover(password, keyObject);
+  return privKey.toString('hex');
+};
+
+console.log(dumpPrivKey(process.argv[2], process.argv[3], process.argv[4]);
